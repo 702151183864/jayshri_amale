e2eba493eb83264c13b9cd96a2e72dc1ce0bb20d
702151183864 <jayshriamale0@gmail.com>
2025-07-31 07:20:02 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed=new Typed(".text",{
-    strings: ["Web Developer" , "Frontend Developer" , "Backend Developer" , "Android Developer"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 300,
-    loop: true
-});
